cc82bd5e6b104417c997e769ebc3ec33c0b7dc13
igweemechi <114313571+igweemechi@users.noreply.github.com>
2024-07-17 01:24:28 +0100

    Add files via upload

---
+++ Google Sheet.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx7wLC25SOo7mPIYFHXBHTNRdKioiljJknJ6h55oO7EoiG8L90AOUlGJnZZtBz215xeOg/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method:'POST', body: new FormData(form)})
+    .then(response => alert("Thank You! Your form is submitted successfully." ))
+    .then(() => { window.location.reload(); })
+    .catch(error => console.error('Error!', error.message))
+})
